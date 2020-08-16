bb0486d4bb0431f8e449da33d13bdb2f87551453
Marcin Kołtoński <koltonskim@gmail.com>
2020-08-17 00:31:22 +0200

    Add class to selected event

--- app/javascript/components/EventList.js
+++ app/javascript/components/EventList.js
@@ -5,15 +5,12 @@ import { Link } from 'react-router-dom';
 class EventList extends React.Component {
 
   renderEvents() {
-    const { events } = this.props;
-    events.sort(
-      (a, b) => new Date(b.event_date) - new Date(a.event_date),
-    );
-
+    const { activeId, events } = this.props;
+    events.sort((a, b) => new Date(b.event_date) - new Date(a.event_date));
 
   return events.map(event => (
     <li key={event.id}>
-      <Link to={`/events/${event.id}`}>
+      <Link to={`/events/${event.id}`} className={activeId === event.id ? 'active' : ''}>
         {event.event_date}
         {' - '}
         {event.event_type}
@@ -33,10 +30,12 @@ class EventList extends React.Component {
 }
 
 EventList.propTypes = {
+  activeId: PropTypes.number,
   events: PropTypes.arrayOf(PropTypes.object),
 };
 
 EventList.defaultProps = {
+  activeId: indefined,
   events: [],
 };
 
